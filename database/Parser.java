9beb6b7592ba6bc320da4c0bd07481e1d90f0722
shuhaokou <shuhao501@yahoo.com>
2015-03-26 09:56:15 -0500

    Changed the parser
    I think it would be cleaner. Added a break in the while loop, so the
    parser now only gives the first professor's information.

--- database/Parser.java
+++ database/Parser.java
@@ -2,6 +2,7 @@ package database;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -28,80 +29,52 @@ public class Parser {
 			line = s.nextLine();
 			
 			if(line.equals("  <item>")){
+				String head = "";
+				String tail = "";
+				
 				String next = s.nextLine();//go to <pic>
-				next = s.nextLine(); // go to <name>
-				professor.setName(getName(next));
 				
-				next = s.nextLine(); //go to <title>
-				professor.setTitle(getTitle(next));
-				
-				next = s.nextLine(); // go to <department>
-				professor.setDepartment(getDepartment(next));
-				
-				next = s.nextLine(); // go to <phone>
-				professor.setPhone(getPhone(next));
-				
-				next = s.nextLine(); // go to <email>
-				professor.setEmail(getEmail(next));
-				
-				next = s.nextLine(); // go to <loc1>
-				professor.setPhone(getLoc1(next));
-				
-				next = s.nextLine(); // go to <loc2>
-				professor.setPhone(getLoc2(next));
-					
+				next = s.nextLine(); 					// go to <name>
+				head = "<name>"; tail = "</name>";
+				professor.setName(getContent(next, head, tail));
+				System.out.println(getContent(next, head, tail));
+				next = s.nextLine(); 					//go to <title>
+				head = "<title>"; tail = "</title>";
+				professor.setTitle(getContent(next, head, tail));
+				System.out.println(getContent(next, head, tail));
+				next = s.nextLine(); 					// go to <department>
+				head = "<department>"; tail = "</department>";
+				professor.setDepartment(getContent(next, head, tail));
+				System.out.println(getContent(next, head, tail));
+				next = s.nextLine(); 					// go to <phone>
+				head = "<phone>"; tail = "</phone>";
+				professor.setPhone(getContent(next, head, tail));
+				System.out.println(getContent(next, head, tail));
+				next = s.nextLine(); 					// go to <email>
+				head = "<email>"; tail = "</email>";
+				professor.setEmail(getContent(next, head, tail));
+				System.out.println(getContent(next, head, tail));
+				next = s.nextLine(); 					// go to <loc1>
+				head = "<location_line1>"; tail = "</location_line1>";
+				professor.setLoc1(getContent(next, head, tail));
+				System.out.println(getContent(next, head, tail));
+				next = s.nextLine();					 // go to <loc2>
+				head = "<location_line2>"; tail = "</location_line2>";
+				professor.setLoc2(getContent(next, head, tail));
+				System.out.println(getContent(next, head, tail));
+				break;
 			}
 		}
 		
 	}
-	private String getName(String s){	// remove <name> and </name>
-		String[] temp = s.split("<name>");
-		temp = temp[1].toString().split("</name>");
+	
+	private String getContent(String s, String head, String tail){	// remove <name> and </name> and so on
+		String[] temp = s.split(head);
+		temp = temp[1].toString().split(tail);
 		s = temp[0].toString();
 		return s;
 	}
 	
-	private String getTitle(String s){	// remove <title> and </title>
-		String[] temp = s.split("<title>");
-		temp = temp[1].toString().split("</title>");
-		s = temp[0].toString();
-		return s;
-	}
-	
-	private String getDepartment(String s){	// remove <department> and </department>
-		String[] temp = s.split("<department>");
-		temp = temp[1].toString().split("</department>");
-		s = temp[0].toString();
-		return s;
-	}
-	
-	private String getPhone(String s){	// remove <phone> and </phone>
-		String[] temp = s.split("<phone>");
-		temp = temp[1].toString().split("</phone>");
-		s = temp[0].toString();
-		return s;
-	}
-	
-	private String getEmail(String s){	// remove <email> and </email>
-		String[] temp = s.split("<email>");
-		temp = temp[1].toString().split("</email>");
-		s = temp[0].toString();
-		return s;
-	}
-	
-	private String getLoc1(String s){	// remove <location_line1> and </location_line1>
-		String[] temp = s.split("<location_line1>");
-		temp = temp[1].toString().split("</location_line1>");
-		s = temp[0].toString();
-		return s;
-	}
-	
-	private String getLoc2(String s){	// remove <location_line2> and </location_line2>
-		String[] temp = s.split("<location_line2>");
-		temp = temp[1].toString().split("</location_line2>");
-		s = temp[0].toString();
-		return s;
-	}
 	
 	
 
