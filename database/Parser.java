0dd58108a2b3449c178780abed53a88f8efe9f41
shuhaokou <shuhao501@yahoo.com>
2015-03-26 09:50:19 -0500

    Added parser
    I am not sure if this is want you want. This parser finds all
    information about a professor then sets everything by using those set
    methods provided in the professor class.

--- database/Parser.java
+++ database/Parser.java
@@ -11,20 +11,99 @@ public class Parser {
 	//Create an ArrayList of type Professor
 	//Create a new Professor object for every <item> - </item> chunk of text
 	String filename = "ProfessorInfo";
-
-	public Scanner read() throws FileNotFoundException{
-		File f = new File(filename);
-		Scanner s = new Scanner(f);
-		while(s.hasNextLine()){ 
-			String next = s.nextLine();
-			System.out.println(next);
-		}
-		return s;
-		
-	}
+	private String line = "";
+	Professor professor = new Professor();
 	
 	public Parser(){
 		
 	}
+	public void read() throws FileNotFoundException{
+		File f = new File(filename);
+		Scanner s = new Scanner(f);
+		parsing(s);
+	}
+	
+	private void parsing(Scanner s){
+		while (s.hasNextLine()){
+			line = s.nextLine();
+			
+			if(line.equals("  <item>")){
+				String next = s.nextLine();//go to <pic>
+				next = s.nextLine(); // go to <name>
+				professor.setName(getName(next));
+				
+				next = s.nextLine(); //go to <title>
+				professor.setTitle(getTitle(next));
+				
+				next = s.nextLine(); // go to <department>
+				professor.setDepartment(getDepartment(next));
+				
+				next = s.nextLine(); // go to <phone>
+				professor.setPhone(getPhone(next));
+				
+				next = s.nextLine(); // go to <email>
+				professor.setEmail(getEmail(next));
+				
+				next = s.nextLine(); // go to <loc1>
+				professor.setPhone(getLoc1(next));
+				
+				next = s.nextLine(); // go to <loc2>
+				professor.setPhone(getLoc2(next));
+				
+				
+			}
+		}
+		
+	}
+	private String getName(String s){	// remove <name> and </name>
+		String[] temp = s.split("<name>");
+		temp = temp[1].toString().split("</name>");
+		s = temp[0].toString();
+		return s;
+	}
+	
+	private String getTitle(String s){	// remove <name> and </name>
+		String[] temp = s.split("<title>");
+		temp = temp[1].toString().split("</title>");
+		s = temp[0].toString();
+		return s;
+	}
+	
+	private String getDepartment(String s){	// remove <name> and </name>
+		String[] temp = s.split("<department>");
+		temp = temp[1].toString().split("</department>");
+		s = temp[0].toString();
+		return s;
+	}
+	
+	private String getPhone(String s){	// remove <name> and </name>
+		String[] temp = s.split("<phone>");
+		temp = temp[1].toString().split("</phone>");
+		s = temp[0].toString();
+		return s;
+	}
+	
+	private String getEmail(String s){	// remove <name> and </name>
+		String[] temp = s.split("<email>");
+		temp = temp[1].toString().split("</email>");
+		s = temp[0].toString();
+		return s;
+	}
+	
+	private String getLoc1(String s){	// remove <name> and </name>
+		String[] temp = s.split("<location_line1>");
+		temp = temp[1].toString().split("</location_line1>");
+		s = temp[0].toString();
+		return s;
+	}
+	
+	private String getLoc2(String s){	// remove <name> and </name>
+		String[] temp = s.split("<location_line2>");
+		temp = temp[1].toString().split("</location_line2>");
+		s = temp[0].toString();
+		return s;
+	}
+	
+	
 
 }
