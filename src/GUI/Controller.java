fa8242a538ba680161e7b0a38f38e7c6c2a882ed
shuhaokou <shuhao501@yahoo.com>
2015-03-26 09:51:35 -0500

    Revert "src"
    This reverts commit 551a3debc88f2b3e0dd8aa611b9683b825681a6f.

---
+++ src/GUI/Controller.java
@@ -0,0 +1,23 @@
+package GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SplitMenuButton;
+
+
+public class Controller {
+	
+	@FXML
+	SplitMenuButton building;
+	
+	@FXML
+	SplitMenuButton department;
+	
+	@FXML
+	ListView<String> filteredNames;
+	
+	
+	
+	
+
+}
