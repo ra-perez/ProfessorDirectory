a2a6f65ebeb12079dc3df2bc64a7a242405195de
shuhaokou <shuhao501@yahoo.com>
2015-03-26 09:25:30 -0500

    nothing

--- src/database/Parser.java
+++ src/database/Parser.java
@@ -1,8 +1,15 @@
 package database;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Parser {
 	//Go through information in TXT file
 	//Create an ArrayList of type Professor
 	//Create a new Professor object for every <item> - </item> chunk of text
+	String filename = "ProfessorInfo.txt";
+	File f = new File(filename);
+	Scanner s = new Scanner(f);
 	
+
 }
