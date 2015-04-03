a2f50ca1f724efd77c6ee50855d2c5765bf385d9
ra-perez <PerezRA@hendrix.edu>
2015-04-02 20:44:47 -0500

    Added Professor Class, Added comments to Parser

---
+++ src/database/Parser.java
@@ -0,0 +1,8 @@
+package database;
+
+public class Parser {
+	//Go through information in TXT file
+	//Create an ArrayList of type Professor
+	//Create a new Professor object for every <item> - </item> chunk of text
+	
+}
