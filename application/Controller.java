5d518f13a3904d20709fa978f35c1072deaf51b7
shuhaokou <shuhao501@yahoo.com>
2015-04-05 02:03:50 -0500

    Move Gui from GUI package to application package

---
+++ application/Controller.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import database.Database;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SplitMenuButton;
+
+
+public class Controller {
+	
+	@FXML
+	SplitMenuButton building;
+	@FXML
+	SplitMenuButton department;
+	@FXML
+	ListView<String> filteredNames;
+	//Database professorDB;
+	ArrayList<String> buildingList;
+	ArrayList<String> departmentList;
+	ArrayList<String> filteredNamesList;
+	
+	@FXML
+	private void initialize() throws ClassNotFoundException, SQLException {
+		//professorDB.initializeDatabase();
+		//buildingList = professorDB.getBuildings();
+		//departmentList = professorDB.getDepartments();
+		//filteredNamesList = professorDB.getFilteredNames("", "");
+	}
+}
--- application/Main.java
+++ application/Main.java
@@ -1,7 +1,11 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,15 +14,27 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("Gui.fxml"));
+			BorderPane root = (BorderPane) loader.load();
+			Scene scene = new Scene(root,409,471);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+		        @Override
+		        public void handle(WindowEvent t) {
+		            Platform.exit();
+		            System.exit(0);
+		        }
+		    }); 
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
 	
 	public static void main(String[] args) {
 		launch(args);
