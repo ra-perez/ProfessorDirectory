94556afad348ff76d97682ede3b7246685e23fc3
shuhaokou <shuhao501@yahoo.com>
2015-04-05 02:28:44 -0500

    Added Textparser
    This class checks if the the parser does find correct information. And
    the parser works

--- database/Parser.java
+++ database/Parser.java
@@ -13,7 +13,9 @@ public class Parser {
 	//Create a new Professor object for every <item> - </item> chunk of text
 	String filename = "ProfessorInfo";
 	private String line = "";
+	private String next = "";
 	Professor professor = new Professor();
+	
 	
 	public Parser(){
 		
@@ -32,50 +34,48 @@ public class Parser {
 				String head = "";
 				String tail = "";
 				
-				String next = s.nextLine();//go to <pic>
+				next = s.nextLine();//go to <pic>
 				
 				next = s.nextLine(); 					// go to <name>
 				head = "<name>"; tail = "</name>";
 				professor.setName(getContent(next, head, tail));
-				System.out.println(getContent(next, head, tail));
+
 				next = s.nextLine(); 					//go to <title>
 				head = "<title>"; tail = "</title>";
 				professor.setTitle(getContent(next, head, tail));
-				System.out.println(getContent(next, head, tail));
+
 				next = s.nextLine(); 					// go to <department>
 				head = "<department>"; tail = "</department>";
 				professor.setDepartment(getContent(next, head, tail));
-				System.out.println(getContent(next, head, tail));
+
 				next = s.nextLine(); 					// go to <phone>
 				head = "<phone>"; tail = "</phone>";
 				professor.setPhone(getContent(next, head, tail));
-				System.out.println(getContent(next, head, tail));
+
 				next = s.nextLine(); 					// go to <email>
 				head = "<email>"; tail = "</email>";
 				professor.setEmail(getContent(next, head, tail));
-				System.out.println(getContent(next, head, tail));
+
 				next = s.nextLine(); 					// go to <loc1>
 				head = "<location_line1>"; tail = "</location_line1>";
 				professor.setLoc1(getContent(next, head, tail));
-				System.out.println(getContent(next, head, tail));
+
 				next = s.nextLine();					 // go to <loc2>
 				head = "<location_line2>"; tail = "</location_line2>";
 				professor.setLoc2(getContent(next, head, tail));
-				System.out.println(getContent(next, head, tail));
-				break;
+
+				
 			}
 		}
 		
 	}
 	
-	private String getContent(String s, String head, String tail){	// remove <name> and </name> and so on
+	public String getContent(String s, String head, String tail){	// remove <name> and </name> and so on
 		String[] temp = s.split(head);
 		temp = temp[1].toString().split(tail);
 		s = temp[0].toString();
 		return s;
 	}
-	
-	
 	
 
 }
--- database/TestParser.java
+++ database/TestParser.java
@@ -1,15 +1,59 @@
 package database;
 
-import java.io.FileNotFoundException;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class TestParser {
 	Parser p = new Parser();
+	private String head = "";
+	private String tail = "";
 	@Test
-	public void testreadingfile() throws FileNotFoundException {
-		p.read();
-		
+	public void testgetContentName()  {
+		head = "<name>";
+		tail = "</name>";
+		assertTrue(p.getContent("    <name>Susan Ablondi</name>", head, tail).equals("Susan Ablondi"));
+	}
+	@Test
+	public void testgetContentTitle() {
+		head = "<title>";
+		tail = "</title>";
+		assertTrue(p.getContent("    <title>Noyce Program Manager</title>", head, tail).equals("Noyce Program Manager"));
+	}
+	
+	@Test
+	public void testgetContentDepartment(){
+		head = "<department>";
+		tail = "</department>";
+		assertTrue(p.getContent("    <department>Education</department>", head, tail).equals("Education"));
+	}
+	
+	@Test
+	public void testgetContentPhone(){
+		head = "<phone>";
+		tail = "</phone>";
+		assertTrue(p.getContent("    <phone>(501) 450-1379</phone>", head, tail).equals("(501) 450-1379"));
+	}
+	
+	@Test
+	public void testgetContentEmail(){
+		head = "<email>";
+		tail = "</email>";
+		assertTrue(p.getContent("    <email>ablondis@hendrix.edu</email>", head, tail).equals("ablondis@hendrix.edu"));
+	}
+	
+	@Test
+	public void testgetContentloc1(){
+		head = "<location_line1>";
+		tail = "</location_line1>";
+		assertTrue(p.getContent("    <location_line1>Education Department</location_line1>", head, tail).equals("Education Department"));
+	}
+	
+	@Test
+	public void testgetContentloc2(){
+		head = "<location_line2>";
+		tail = "</location_line2>";
+		assertTrue(p.getContent("    <location_line2>Mills 105</location_line2>", head, tail).equals("Mills 105"));
 	}
 
 }
