b23b42a8b619aa37b7f53eeb7667ebb6fd6452bc
ra-perez <PerezRA@hendrix.edu>
2015-04-05 20:28:55 -0500

    getFilteredNames() in Database added and work on Controller

--- GUI/Controller.java
+++ GUI/Controller.java
@@ -1,5 +1,9 @@
 package GUI;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import database.Database;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SplitMenuButton;
@@ -9,15 +13,20 @@ public class Controller {
 	
 	@FXML
 	SplitMenuButton building;
-	
 	@FXML
 	SplitMenuButton department;
-	
 	@FXML
 	ListView<String> filteredNames;
+	Database professorDB;
+	ArrayList<String> buildingList;
+	ArrayList<String> departmentList;
+	ArrayList<String> filteredNamesList;
 	
-	
-	
-	
-
+	@FXML
+	private void initialize() throws ClassNotFoundException, SQLException {
+		professorDB.initializeDatabase();
+		buildingList = professorDB.getBuildings();
+		departmentList = professorDB.getDepartments();
+		filteredNamesList = professorDB.getFilteredNames("", "");
+	}
 }
--- database/Database.java
+++ database/Database.java
@@ -10,7 +10,7 @@ public class Database {
 		professors = p;
 	}
 	
-	private void initializeDatabase() throws ClassNotFoundException, SQLException {
+	public void initializeDatabase() throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
         Connection con = DriverManager.getConnection("jdbc:sqlite:professors.db");
         Statement stat = con.createStatement();
@@ -29,12 +29,12 @@ public class Database {
 	        );
 		}
 	}
-		
-	public ArrayList<String> getColInfo(String column) throws ClassNotFoundException, SQLException {
+	
+	private ArrayList<String> makeQuery(String query) throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
         Connection con = DriverManager.getConnection("jdbc:sqlite:testdb");
         Statement stat = con.createStatement();
-        ResultSet rs = stat.executeQuery("SELECT " + column + " FROM Professors");
+        ResultSet rs = stat.executeQuery(query);
         ArrayList<String> toReturn = new ArrayList<String>();
         while (rs.next()) {
         	toReturn.add(rs.getString("Building"));
@@ -42,7 +42,27 @@ public class Database {
         return toReturn;
 	}
 	
+	private ArrayList<String> getColInfo(String column) throws ClassNotFoundException, SQLException {
+        return makeQuery("SELECT " + column + " FROM Professors");
+	}
+	
 	public ArrayList<String> getBuildings() throws ClassNotFoundException, SQLException {return getColInfo("Building");}
 	public ArrayList<String> getDepartments() throws ClassNotFoundException, SQLException {return getColInfo("Department");}
+		
+	//Give an empty string for input variables if not filtered on that information
+	//So getFilteredNames("", "") returns all Professor Names
+	public ArrayList<String> getFilteredNames(String building, String department) throws ClassNotFoundException, SQLException {
+		String query = "";
+		if (department.equals("")) {
+			query = "SELECT Name FROM Professors WHERE Building = '" + building + "'";
+		} else if (building.equals("")) {
+			query = "SELECT Name FROM Professors WHERE Department = '" + department + "'";
+		} else if (!department.equals("") && !building.equals("")) {
+			query = "SELECT Name FROM Professors WHERE Building = '" + building + "' AND WHERE Department = '" + department + "'";
+		} else {
+			query = "SELECT Name FROM Professors";
+		}
+		return makeQuery(query);
+	}
 
 }
