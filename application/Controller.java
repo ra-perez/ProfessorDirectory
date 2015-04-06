536a2a79d06b6fe6398b3fe504b82f4a56a652be
ra-perez <PerezRA@hendrix.edu>
2015-04-05 21:06:32 -0500

    Deleted duplicate file

--- application/Controller.java
+++ application/Controller.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.io.FileNotFoundException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 import database.Database;
+import database.Parser;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SplitMenuButton;
@@ -17,16 +19,23 @@ public class Controller {
 	SplitMenuButton department;
 	@FXML
 	ListView<String> filteredNames;
-	//Database professorDB;
+	Database professorDB;
 	ArrayList<String> buildingList;
 	ArrayList<String> departmentList;
 	ArrayList<String> filteredNamesList;
 	
 	@FXML
 	private void initialize() throws ClassNotFoundException, SQLException {
-		//professorDB.initializeDatabase();
-		//buildingList = professorDB.getBuildings();
-		//departmentList = professorDB.getDepartments();
-		//filteredNamesList = professorDB.getFilteredNames("", "");
+		Parser p = new Parser();
+		try {
+			p.read();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		professorDB = new Database(p.getProfessors());
+		buildingList = professorDB.getBuildings();
+		departmentList = professorDB.getDepartments();
+		filteredNamesList = professorDB.getFilteredNames("", "");
 	}
 }
